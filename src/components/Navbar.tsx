4f9d5869687d1ee6427f88a53fc8cef485f3c962
KonsnantinM <mihajlov8@gmail.com>
2025-07-23 00:51:28 +0200

    end

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,6 @@ export default function Navbar() {
           </svg>
         </button>
 
-        {/* Навигация */}
         <div
           className={`${
             isOpen ? 'block' : 'hidden'
